773407f8589920ffc4fad09cc3bbfa3c4b8c0654
Blessing U <blessing986@users.noreply.github.com>
2023-10-31 21:26:30 +0000

    chore: implemented winning combinations

--- src/components/GameBoard.jsx
+++ src/components/GameBoard.jsx
@@ -1,36 +1,7 @@
-const initialGameBoard = [
-  [null, null, null],
-  [null, null, null],
-  [null, null, null],
-];
-
-export default function GameBoard({ onSelectSquare, turns }) {
-  let gameBoard = initialGameBoard;
-
-  for (const turn of turns) {
-    const { square, player } = turn;
-    const { row, col } = square;
-
-    gameBoard[row][col] = player;
-  }
-
-  //   const [gameBoard, setGameBoard] = useState(initialGameBoard);
-
-  //   const handleSelectSquare = (rowIndex, colIndex) => {
-  //     setGameBoard((prevGameBoard) => {
-  //       const updatedBoard = [
-  //         ...prevGameBoard.map((innerArray) => [...innerArray]),
-  //       ];
-  //       updatedBoard[rowIndex][colIndex] = activePlayeSymbol;
-  //       return updatedBoard;
-  //     });
-
-  //     onSelectSquare();
-  //   };
-
+export default function GameBoard({ onSelectSquare, board }) {
   return (
     <ol id="game-board">
-      {gameBoard.map((row, rowIndex) => (
+      {board.map((row, rowIndex) => (
         <li key={rowIndex}>
           <ol>
             {row.map((PlayerSymbol, colIndex) => (
